0e49fb1c7f80f9a04545991a0be780184f87049c
godslove-a <godsloveasiamah2002@gmail.com>
2025-05-23 02:59:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const button = document.getElementById("fetchQuote");
+const quoteText = document.getElementById("quoteText");
+const quoteAuthor = document.getElementById("quoteAuthor");
+const spinner = document.getElementById("spinner");
+
+button.addEventListener("click", () => {
+    spinner.classList.remove("hidden");
+    quoteText.textContent = "";
+    quoteAuthor.textContent = "";
+    
+    fetch("http://api.quotable.io/random")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to fetch quote");
+        }
+        return response.json();
+    })
+    .then(data => {
+        spinner.classList.add("hidden");
+        quoteText.textContent = `"${data.content}"`;
+        quoteAuthor.textContent = `-${data.author}`;
+    })
+    .catch(error => {
+        spinner.classList.add("hidden");
+        quoteText.textContent = "Something went wrong. Try again";
+        quoteAuthor.textContent = "";
+        console.error("Error fetching the quote", error);
+    });
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+        e.preventDefault();
+        button.click();
+    }
+});
